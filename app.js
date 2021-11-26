7efd7637ae8a30ae9448b7b85c8853b4c7f07458
KaniusheuskiArt <konusevskiart@gmail.com>
2021-11-26 22:00:04 +0300

    refactor: change classes and id  according to rule 3.3 and 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -32,18 +32,22 @@ var createNewTaskElement=function(taskString){
   var deleteButton=document.createElement('button');//delete button
   var deleteButtonImg=document.createElement('img');//delete button image
 
+  listItem.className = 'task';
+
   label.innerText=taskString;
-  label.className='task';
+  label.className='task-text task-label';
 
   //Each elements, needs appending
+  checkBox.className = 'task-checkbox'
   checkBox.type='checkbox';
   editInput.type='text';
-  editInput.className='task';
+  editInput.className='task-text task-input task-input-text';
 
   editButton.innerText='Edit'; //innerText encodes special characters, HTML does not.
-  editButton.className='edit';
+  editButton.className='btn edit';
 
-  deleteButton.className='delete';
+  deleteButton.className='btn delete';
+  deleteButtonImg.className = 'delete-img'
   deleteButtonImg.src='./remove.svg';
   deleteButton.appendChild(deleteButtonImg);
 
